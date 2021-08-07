34e01d1fde7722e3f304a06200383a1bbd3d8ae4
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2021-08-07 19:22:25 +0200

    Add FE component

--- frontend/employeemanagerapp/src/app/app.module.ts
+++ frontend/employeemanagerapp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
